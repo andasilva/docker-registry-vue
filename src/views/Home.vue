1dfe79a6c700d3e0df08aaca56143b11c8b59d14
Neto da Silva André <an-dasilva@outlook.com>
2020-11-25 20:35:21 +0100

    Add vue router

---
+++ src/views/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-app>
+    <v-app-bar
+        app
+        color="primary"
+        dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+            transition="scale-transition"
+            width="40"
+        />
+
+        <v-img
+            alt="Vuetify Name"
+            class="shrink mt-1 hidden-sm-and-down"
+            contain
+            min-width="100"
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+            width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+<style>
+#nav a{
+  color: white;
+}
+</style>
